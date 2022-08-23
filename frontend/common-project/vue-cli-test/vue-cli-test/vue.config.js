bbbf680e11424b4e74b013610fd925fa30340eb9
cat-fishing <13342271206@163.com>
2022-08-23 23:04:01 +0800

    vue学习day27：配置代理

--- frontend/common-project/vue-cli-test/vue-cli-test/vue.config.js
+++ frontend/common-project/vue-cli-test/vue-cli-test/vue.config.js
@@ -1,5 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave:false //关闭eslint检查
+  lintOnSave:false, //关闭eslint检查
+
+  //配置代理
+  // devServer:{
+  //   proxy:{
+  //     '/api':{                          //虚拟路径，需要去除
+  //       target:'http://localhost:8080', 
+  //       pathRewrite:{'^/api':''},       //路径重写，匹配api开头的字符串，并把api替换为空字符串
+  //       ws:true,                        //用于支持websocket
+  //       changeOrigin:true               //wei
+  //     }
+  //   }
+  // }
 })
