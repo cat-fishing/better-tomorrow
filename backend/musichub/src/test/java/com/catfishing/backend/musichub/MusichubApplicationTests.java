238d7a4295f3f61a6922f172bd8834a6f23f5d9b
catfishing <13342271206@163.com>
2022-09-13 21:49:20 +0800

    音乐网站后端项目初始化提交

---
+++ backend/musichub/src/test/java/com/catfishing/backend/musichub/MusichubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catfishing.backend.musichub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusichubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
